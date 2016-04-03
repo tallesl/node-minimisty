4ed2037858ac9dcc6974e81166abb3c54a4acef1
Talles L <talleslasmar@gmail.com>
2016-04-02 21:43:09 -0300

    fixed module.exports

---
+++ parse.js
@@ -0,0 +1,80 @@
+'use strict'
+
+module.exports = function (args) {
+  const opts = { _: [ ], _flags: { } }
+  const setFlag = (_flag) => { opts._flags[_flag] = true }
+
+  for (let i = 0; i < args.length; ++i) {
+    const cur = args[i]
+    const n = args[i + 1]
+
+    let parsed = dashEqualArgument(cur)
+    if (parsed) {
+      opts[parsed[0]] = parsed[1]
+      continue
+    }
+
+    parsed = dashArgument(cur, n)
+    if (parsed) {
+      opts[parsed[0]] = parsed[1]
+      ++i
+      continue
+    }
+
+    parsed = singleDashFlag(cur)
+    if (parsed) {
+      parsed.forEach(setFlag)
+      continue
+    }
+
+    parsed = doubleDashFlag(cur)
+    if (parsed) {
+      opts._flags[parsed] = true
+      continue
+    }
+
+    opts._.push(cur)
+  }
+
+  return opts
+}
+
+function firstNonDash (arg) {
+  if (arg[0] === '-') {
+    for (let i = 0; i < arg.length;) {
+      if (arg[++i] !== '-') {
+        return i
+      }
+    }
+  }
+}
+
+function dashEqualArgument (arg) {
+  const first = firstNonDash(arg)
+  if (first) {
+    const tuple = arg.substr(first).split('=')
+    if (tuple[1]) {
+      return tuple
+    }
+  }
+}
+
+function dashArgument (arg, next) {
+  const first = firstNonDash(arg)
+  if (first && next && first !== arg.length && !firstNonDash(next)) {
+    return [ arg.substr(first), next ]
+  }
+}
+
+function singleDashFlag (arg) {
+  if (firstNonDash(arg) === 1) {
+    return arg.substr(1).split('')
+  }
+}
+
+function doubleDashFlag (arg) {
+  const first = firstNonDash(arg)
+  if (first && first !== arg.length) {
+    return arg.substr(first)
+  }
+}
--- test.js
+++ test.js
@@ -3,28 +3,23 @@
 /* global it */
 
 const assert = require('assert')
-const args = '-x 3 -y 4 -n5 -abc --beep=boop foo bar baz'
-const minimisty = require('./')
+const parse = require('./parse')
 
-it(args, () => {
-  // arrange
-  const expected = {
-    _: [ 'foo', 'bar', 'baz' ],
-    _flags: {
-      n: true,
-      5: true,
-      a: true,
-      b: true,
-      c: true
-    },
-    x: '3',
-    y: '4',
-    beep: 'boop'
-  }
-
-  // act
-  const actual = minimisty.parse(args.split(' '))
-
-  // assert
-  assert.deepEqual(actual, expected)
+it('should parse correctly', () => {
+  assert.deepEqual(
+    parse('-x 3 -y 4 -n5 -abc --beep=boop foo bar baz'.split(' ')),
+    {
+      _: [ 'foo', 'bar', 'baz' ],
+      _flags: {
+        n: true,
+        5: true,
+        a: true,
+        b: true,
+        c: true
+      },
+      x: '3',
+      y: '4',
+      beep: 'boop'
+    }
+  )
 })
