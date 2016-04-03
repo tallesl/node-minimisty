f0c43832fa1399946bf45acb543475326a4a4b8b
Talles L <talleslasmar@gmail.com>
2016-04-02 21:47:59 -0300

    fixed benchmark

--- benchmark.js
+++ benchmark.js
@@ -5,16 +5,16 @@
 const args = '-x 3 -y 4 -n5 -abc --beep=boop foo bar baz'.split(' ')
 const benchmark = new (require('benchmark')).Suite
 const minimist = require('minimist')
-const minimisty = require('.')
+const minimisty = require('./parse')
 
 benchmark.add('minimist', () => {
   minimist(args)
 }).add('minimisty', () => {
-  minimisty.parse(args)
+  minimisty(args)
 }).on('cycle', (e) => {
   console.log(String(e.target))
 }).on('complete', () => {
-  console.log(benchmark.filter('fastest').pluck('name') + 'is the fastest')
+  console.log(benchmark.filter('fastest').map('name') + ' is the fastest')
 }) .run(
   { 'async': true }
 )
